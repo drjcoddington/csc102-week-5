13d9dc73dacaf7996a9980aa7f182954d1ac92ff
drjcoddington <drjcoddington@yahoo.com>
2024-11-13 13:07:39 -0700

    Add files via upload

---
+++ CSC102 11 13 24 nav.js
@@ -0,0 +1,15 @@
+function replace()
+{
+    location.replace("csc102 11 13 24 table.html");
+}
+
+
+function reload()
+{
+    location.reload();
+}
+
+function addNew()
+{
+    window.open("move imageV2.html");
+}
